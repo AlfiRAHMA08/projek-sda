ca327321c09e2f4786d821b39abeedcc3b634f58
bara <106955611+baralul@users.noreply.github.com>
2024-06-15 21:30:59 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  return 0;
+}
+
+/*
+  Ide:
+    - buat login : deskripsi deskripsi deskripsi
+    - ide 2 : deskripsi deskripsi deskripsi
+    - ide 3 : deskripsi deskripsi deskripsi
+*/
